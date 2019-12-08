ce1e44ece38929d5b5f665b6a33077f9cdf5ae30
Richard Li <richardli125@gmail.com>
2019-12-07 19:30:18 -0500

    started testing different functions with different matchers

---
+++ functions.test.js
@@ -0,0 +1,72 @@
+const functions = require('./functions');
+
+// toBe
+test('Properly adds two numbers', () => {
+  expect(functions.add(1, 2)).toBe(3);
+});
+
+// not.toBe
+test('Does not properly add', () => {
+  expect(functions.add(1, 2)).not.toBe(13);
+});
+
+// toBeNull
+test('Checks to see if null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test('Checks to see if false', () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test('User should be Richard li object', () => {
+  expect(functions.createUser()).toStrictEqual({
+    firstName: 'Richard',
+    lastName: 'Li'
+  });
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// toBe is used for primitive types
+
+// Less than and greater than
+test('Should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+
+// Regex
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+  usernames = ['john', 'karen', 'admin'];
+  expect(usernames).toContain('admin');
+});
+
+// Working with async data
+
+// Promise
+// test('User fetched name should be Leanne Graham', () => {
+//   expect.assertions(1);
+//   return functions.fetchUser().then(data => {
+//     expect(data.name).toEqual('Leanne Graham1');
+//   });
+// });
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
+});
